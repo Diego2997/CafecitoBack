bb71a0388c620773cb33ab0731688cc15fc438de
Diego Mercado <diego.mercado1997@hotmail.com>
2023-06-22 11:32:41 -0300

    agregando middlewares

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from 'express'
+import cors from 'cors'
+import morgan from 'morgan'
+import dotenv from 'dotenv'
 
 //Tomar un puerto
 //crear una instancia de express
@@ -9,5 +12,7 @@ app.listen(app.get("port"),()=>{
     console.log("Estoy en el puerto " + app.get("port"))
 })
 //middlewares
-
+app.use(cors())//permite conexiones remotas
+app.use(express.json())//tomar del objeto request datos en formato json
+app.use(morgan("dev"))
 //rutas
