798c8338c4848195e88a8b04db445cf85f4c8d32
Diego Mercado <diego.mercado1997@hotmail.com>
2023-07-05 21:53:56 -0300

    se agrega la validacion de un usuario usando express validator

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { crearUsuario, obtenerUnUsuario, obtenerUsuarios } from "../controllers/usuarios.controllers";
+import validarUsuario from "../helpers/validarUsuario";
 
 const router = Router()
 
 router.get("/usuarios",obtenerUsuarios)
 router.get("/usuarios/:id",obtenerUnUsuario)
-router.post("/usuarios",crearUsuario)
+router.post("/usuarios",validarUsuario,crearUsuario)
 
 export default router
